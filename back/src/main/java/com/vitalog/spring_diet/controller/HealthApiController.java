20a2fc64a13e6911eaafce8c6f91fa6e326ed0d9
hipkikomori <nerd7439@gmail.com>
2025-08-22 18:17:19 +0900

    추천 운동 페이지 api

---
+++ back/src/main/java/com/vitalog/spring_diet/controller/HealthApiController.java
@@ -0,0 +1,29 @@
+package com.vitalog.spring_diet.controller;
+
+import com.vitalog.spring_diet.dto.HealthDTO; // HealthDTO import
+import com.vitalog.spring_diet.service.HealthService; // HealthService import
+import lombok.RequiredArgsConstructor; // final 필드 생성자를 자동으로 만들어주는 Lombok 어노테이션
+import org.springframework.web.bind.annotation.CrossOrigin; // 다른 출처의 요청을 허용 (CORS 설정)
+
+import org.springframework.web.bind.annotation.GetMapping; // HTTP GET 요청 처리 어노테이션
+import org.springframework.web.bind.annotation.RequestMapping; // 클래스 레벨에서 공통 URL 경로를 지정
+import org.springframework.web.bind.annotation.RestController; // REST API 컨트롤러임을 선언
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000") // http://localhost:3000 요청 허용
+@RequestMapping("/api/health") // 이 컨트롤러의 모든 요청은 '/api/health'로 시작
+@RequiredArgsConstructor
+public class HealthApiController {
+    // 서비스 로직을 사용하기 위해 HealthService를 주입받음
+    private final HealthService healthService;
+
+    // '/api/health/data' 경로의 GET 요청 처리
+    @GetMapping("/data")
+    // 운동 데이터 리스트를 반환하는 메서드
+    public List<HealthDTO> getHealthData() {
+        // 서비스의 메서드 호출하고 결과를 바로 반환
+        return healthService.getHealthData();
+    }
+}
