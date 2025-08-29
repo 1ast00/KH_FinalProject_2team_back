5322db76b6d5e246f5d7aee14bc08ad5118a9d54
scis <scis@kakao.com>
2025-08-29 19:06:09 +0900

    xml 수정

--- src/main/java/com/vitalog/spring_diet/dto/BoardReviewDTO.java
+++ src/main/java/com/vitalog/spring_diet/dto/BoardReviewDTO.java
@@ -3,7 +3,6 @@ package com.vitalog.spring_diet.dto;
 import lombok.Data;
 import org.apache.ibatis.type.Alias;
 
-import java.sql.Clob;
 import java.sql.Date;
 
 @Data
@@ -12,7 +11,7 @@ public class BoardReviewDTO {
     private int brno;
     private int mno;
     private String brtitle;
-    private Clob brcontent;
+    private String brcontent;  //update 25.08.29
     private Date brwrite_date;
     private Date brwrite_update;
     private int brhitcount;
